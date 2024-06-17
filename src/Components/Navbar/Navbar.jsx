c670f2df751a22bfc59301fee2c5e398a76709c0
Fredrick <mwangikingz94@gmail.com>
2024-06-18 00:42:34 +0300

    Nav bar commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,19 @@ import './Navbar.css'
 export const Navbar = () => {
   return (
     <div className='navbar'>
-        
+      <div className="nav-logo">
+        {/* <img src={logo} alt="" /> */}
+        <p>SHOPPER</p>
+        </div>        
+        <ul className="nav-menu">
+        <li>Shop<hr/></li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+        </ul>
+        <div className="nav-login-cart">
+          <button>Login</button>
+        </div>
     </div>
   )
 }
